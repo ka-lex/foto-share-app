133375105344d25b7d22af80eb38697f5c7d089a
Alex <karlsruhealex@gmail.com>
2017-03-16 20:58:47 +0100

    First commit

--- app/controllers/slide_show_controller.rb
+++ app/controllers/slide_show_controller.rb
@@ -1,21 +1,22 @@
 class SlideShowController < ApplicationController
   def index
     # get list of images
+    @images = Dir.glob("#{Rails.root}/app/assets/images/upload/*.jpg")
+
+    @images = @images.shuffle.collect do |image|
+      view_context.image_path "upload/#{image.split('/').last}"
+    end
+
   end
 
   def show
     @images = Dir.glob("#{Rails.root}/app/assets/images/upload/*.jpg")
 
     @images = @images.shuffle.collect do |image|
-      "upload/#{image.split('/').last}"
+      view_context.image_path "upload/#{image.split('/').last}"
     end
 
-    p @images
+    render :json => { :images => @images }
 
-
-    respond_to do |format|
-        format.html { render :layout => !request.xhr? }
-        # other formats
     end
-  end
 end
